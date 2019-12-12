3f8ff02ab28c0e90283695adab50748b54aeb055
Claudio <claudio.moya@rakuten.com>
2019-12-12 17:00:50 +0900

    Added - NPM distribution

--- npm/register/register.development.js
+++ npm/register/register.development.js
@@ -1,6 +1,6 @@
 /*!
  * 
- * @license @rakuten-rex/storybook-inspect-html v1.2.0 2019-12-12
+ * @license @rakuten-rex/storybook-inspect-html v1.2.1 2019-12-12
  * register/register.development.js
  * 
  * Copyright (c) 2018-present, Rakuten, Inc.
