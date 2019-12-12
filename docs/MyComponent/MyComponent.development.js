66f45b39b47ebcd63405a9348fe5ca5fae1f1852
Claudio <claudio.moya@rakuten.com>
2019-12-12 17:00:49 +0900

    Added - Docs for github pages and README file

--- docs/MyComponent/MyComponent.development.js
+++ docs/MyComponent/MyComponent.development.js
@@ -1,6 +1,6 @@
 /*!
  * 
- * @license @rakuten-rex/storybook-inspect-html v1.2.0 2019-12-12
+ * @license @rakuten-rex/storybook-inspect-html v1.2.1 2019-12-12
  * MyComponent/MyComponent.development.js
  * 
  * Copyright (c) 2018-present, Rakuten, Inc.
